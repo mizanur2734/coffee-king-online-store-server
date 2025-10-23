16387f45c85ce1ac25b3f0ed104bf55a1f3ce3e0
mizanur2734 <mizanurbb2791@gmail.com>
2025-10-23 23:24:39 +0600

    Added like/dislike feature for coffees

--- index.js
+++ index.js
@@ -21,31 +21,75 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 async function run() {
   try {
     const database = client.db("coffee-king");
-    const coffeeCollection = database.collection('coffees')
+    const coffeeCollection = database.collection("coffees");
 
     // all coffee
-    app.get("/coffees", async(req, res) =>{
-        const allCoffees = await coffeeCollection.find().toArray()
-        res.send(allCoffees)
-    })
+    app.get("/coffees", async (req, res) => {
+      const allCoffees = await coffeeCollection.find().toArray();
+      res.send(allCoffees);
+    });
 
     // save a coffee data in the database though post request
-    app.post("/add-coffee", async (req, res) =>{
-        const coffeeData = req.body;
-        console.log(coffeeData)
-        const result = await coffeeCollection.insertOne(coffeeData)
-        res.send(result)
-    })
+    app.post("/add-coffee", async (req, res) => {
+      const coffeeData = req.body;
+      console.log(coffeeData);
+      const result = await coffeeCollection.insertOne(coffeeData);
+      res.send(result);
+    });
 
     // get a signgle coffe by id
-     app.get("/coffee/:id", async(req, res) =>{
-        const id = req.params.id
-        const filter = { _id: new ObjectId(id)}
-        const coffee = await coffeeCollection.findOne(filter)
-        console.log(coffee)
-        res.send(coffee)
-    })
+    app.get("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const coffee = await coffeeCollection.findOne(filter);
+      console.log(coffee);
+      res.send(coffee);
+    });
 
+    // get a single coffee data by id
+    app.get("/my-coffees/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email };
+      const coffees = await coffeeCollection.find(filter).toArray();
+      console.log(coffees);
+      res.send(coffees);
+    });
+
+    // handle like toggle
+    app.patch("/like/:coffeeId", async (req, res) => {
+      const id = req.params.coffeeId;
+      const email = req.body.email;
+      const filter = { _id: new ObjectId(id) };
+      const coffee = await coffeeCollection.findOne(filter);
+      // if the email already exists in the likedBy array, remove it (dislike)
+      const alreadyLiked = coffee?.likedBy.includes(email);
+
+      console.log(
+        "akdom surute like er obsta -----> alreadyLike:",
+        alreadyLiked
+      );
+
+      const updateDoc = alreadyLiked
+        ? {
+            // dislike coffee (pop email from likedBy array)
+            $pull: { likedBy: email },
+          }
+        : {
+            // like coffee (pop email from likedBy array)
+            $addToSet: { likedBy: email },
+          };
+      await coffeeCollection.updateOne(filter, updateDoc);
+
+      console.log(
+        "akdom sheshe like er obsta -----> alreadyLike:",
+        !alreadyLiked
+      );
+
+      res.send({
+        message: alreadyLiked ? "dislike successful" : "Like Successful",
+        liked: !alreadyLiked,
+      });
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
